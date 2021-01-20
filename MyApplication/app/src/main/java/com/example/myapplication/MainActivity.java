9c7a7f77bc65aac922f40965e558ef39c66f82fd
Zoya Rashid <70578962+ZoyaRashid@users.noreply.github.com>
2021-01-20 15:17:41 +0500

    initial commit

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onBtnClick(View view) {
+        TextView text = findViewById(R.id.textMessage);
+        text.setText("Hello Have a wonderful day!");
+    }
+}
